675700854d6fa5fc6d7d1fc77dc1db66931d5b0d
Sara Patel <jatinbalar348@gmail.com>
2024-05-01 16:09:58 -0400

    fourth commit

--- src/components/todo/security/msalConfig.js
+++ src/components/todo/security/msalConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
       clientId: '5af04bb2-f6f5-48fb-a9a7-19306e1ca73b', // This is the ONLY mandatory field that you need to supply.
       authority: 'https://jatinbalar1.b2clogin.com/jatinbalar1.onmicrosoft.com/b2c_1_susi', // Choose SUSI as your default authority.
       knownAuthorities: ['jatinbalar1.b2clogin.com'], // Mark your B2C tenant's domain as trusted.
-      redirectUri: 'https://jatinbalar.azurewebsites.net/', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
+      redirectUri: 'https://jatinbalar.azurewebsites.net', // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
       // postLogoutRedirectUri: 'http://localhost:3000', // Indicates the page to navigate after logout.
       navigateToLoginRequestUrl: false, // If 'true', will navigate back to the original request location before processing the auth code response.
     },
