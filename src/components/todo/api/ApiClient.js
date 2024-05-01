387880c2a58d700121cf5e991cdf795b8f11a911
Sara Patel <jatinbalar348@gmail.com>
2024-05-01 16:53:54 -0400

    six commit

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const apiClient = axios.create(
     {
-        baseURL: 'http://172.191.71.65:8080/todo'
+        baseURL: 'http://backendapp123.eastus.cloudapp.azure.com:8080/todo'
     }
 );
